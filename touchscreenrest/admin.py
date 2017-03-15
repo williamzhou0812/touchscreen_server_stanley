4e045a8145303d10159047ded88212c3b6a1d070
Jonathan Adhitama Wongsodihardjo <jonathan@johnbatman.com.au>
2017-03-15 15:21:28 +1100

    Add admin interface and configurations for each Model
    TODOs:
    -Add some dummy data
    -Do extensive testings with dummy data for RESTful API

--- touchscreenrest/admin.py
+++ touchscreenrest/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from touchscreenrest.models import Activity, Destination, Period, Event,Restaurant, Tour, Accomodation, Map,\
+    Advertisement, Image, Video
 
-# Register your models here.
+admin.site.register(Activity)
+admin.site.register(Destination)
+admin.site.register(Period)
+admin.site.register(Event)
+admin.site.register(Restaurant)
+admin.site.register(Tour)
+admin.site.register(Accomodation)
+admin.site.register(Map)
+admin.site.register(Advertisement)
+admin.site.register(Image)
+admin.site.register(Video)
